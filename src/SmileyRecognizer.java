75b98fd7e4d392437965685fdd56e4f511a698da
Petter Poucette <id15ppe@cs.umu.se>
2017-10-11 12:57:25 +0200

    we are screwed

--- src/SmileyRecognizer.java
+++ src/SmileyRecognizer.java
@@ -2,13 +2,17 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class SmileyRecognizer {
     private ASCIIreader asciiReader;
     private ArrayList<Perceptron> perceptrons;
+    private ArrayList<Double[]> activationValues;
+    private double learningValue;
 
     public SmileyRecognizer(File imageFile,File answerFile,File testFile) throws IOException {
+        this.learningValue = 0.05;
         initializeImages(imageFile,answerFile);
         initializeWeights();
     }
@@ -24,14 +28,46 @@ public class SmileyRecognizer {
     }
 
     public void run(){
+        calculateActivationValues();
+    }
+    private void calculateActivationValues(){
+        Image image;
+
+        for (int i = 0; i < asciiReader.getTrainingImages().size(); i++){
+            activationValues.add(new Double[4]);
+            image = asciiReader.getTrainingImages().get(i);
+            for (int j = 0 ; j < activationValues.get(i).length; j++){
+                activationValues.get(i)[j] = activationFunction.apply(
+                        sumWWeights(j, image));
+            }
+            adjustWeights(activationValues.get(i),image);
+            System.out.println("Image " + i + " CorrectValue: " + image.getEmotion()
+                    + " Weighted values: "
+                    + "1: " + activationValues.get(i)[0]
+                    + "2: " + activationValues.get(i)[1]
+                    + "3: " + activationValues.get(i)[2]
+                    + "4: " + activationValues.get(i)[3]);
+        }
+    }
+
+    private void adjustWeights(Double[] activationValues,Image image){
+        for(int i=0;i<perceptrons.size();i++){
+            for(int j=0;j<4;j++){
+                if((j+1)==image.getEmotion())
+                    perceptrons.get(i).adjustWeight(j,learningValue,1,activationValues[j]);
+                else
+                    perceptrons.get(i).adjustWeight(j,learningValue,0,activationValues[j]);
+            }
+
+        }
 
     }
 
-    public void initializeImages(File imageFile,File answerFile) throws IOException {
+    private void initializeImages(File imageFile,File answerFile) throws IOException {
         asciiReader=new ASCIIreader(imageFile,answerFile, 2.0/3.0);
     }
 
-    public void initializeWeights(){
+    private void initializeWeights(){
         for(int i=0 ; i<402 ; i++){
             perceptrons.add(new Perceptron());
         }
